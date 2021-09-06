6dd78b0aa0bec8e82bc095ade5bce70f86451f7c
Harshit Jain <50293718+harshitjain0510@users.noreply.github.com>
2021-09-06 12:52:21 +0530

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -20,21 +20,22 @@ signed main(){
 		cin.ignore(numeric_limits<streamsize>::max(),'\n');
 		cout<<'\n';
 	}
-	double mn=1.0*(1e9),mx=0;
+	double mn=1.0*(1<<30)-1.0,mx=0.0,ans=0.0;
 	for(int i=0; i<game.n;i++){
 		if(game.takeInput()){
 			if(!game.bmk)cout<<"Validating Sudoku...\n\n";
 			if(game.validate()){
 				if(!game.bmk)cout<<"Validated Succesfully..\n\n";
-				clock_t start_t = clock();
+				
 				game.solve();
-				clock_t end_t = clock();
-				cout<<'\n'<<"Hurray!!\nSolved in: "<<(double) (end_t - start_t) / CLOCKS_PER_SEC<<" sec\n";
-				if(game.bmk){
-					double ans=(double) (end_t - start_t) / CLOCKS_PER_SEC;
-					mn=min(ans,mn);
-					mx=max(ans,mx);
-					game.time+=ans;
+
+				if(game.dlx.isSolved){
+					cout<<'\n'<<"Hurray!!\nSolved in: "<<(double)game.time<<" sec\n";
+				}
+				if(game.bmk){;
+					mn=min(game.time,mn);
+					mx=max(game.time,mx);
+					ans+=game.time;
 				}
 			}else 
 				cout<<"Invalid Sudoku...\n\n";
@@ -43,7 +44,7 @@ signed main(){
 	}
 
 	if(game.bmk){
-		cout<<"Solved "<<game.n<<" sudoku's on average time of "<<game.time/game.n<<" sec."<<'\n';
+		cout<<"Solved "<<game.n<<" sudoku's on average time of "<<(double)ans/game.n<<" sec."<<'\n';
 		cout<<"Min. time: "<<mn<<" sec."<<'\n';
 		cout<<"Max. time: "<<mx<<" sec."<<'\n';
 	}
